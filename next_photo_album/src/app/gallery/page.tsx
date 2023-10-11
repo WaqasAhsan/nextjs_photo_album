a847a8023819f06dcb2d8b62d09184267df38f2c
waqas <waqasahsan84@gmail.com>
2023-10-11 11:56:28 +0500

    Gallery page showing images

--- next_photo_album/src/app/gallery/page.tsx
+++ next_photo_album/src/app/gallery/page.tsx
@@ -9,17 +9,17 @@ type SearchResults = {
 export default async function GalleryPage() {
   const results = (await cloudinary.v2.search
     .expression("resource_type:image")
-    .sort_by("public_id", "desc")
-    .max_results(10)
+    .sort_by("created_at", "desc")
+    .max_results(4)
     .execute()) as { resources: SearchResults[] };
   // .then((result: any) => console.log(result));
   console.log(results);
 
   return (
     <section>
-      <div className="">
+      <div className="flex flex-col gap-8">
         <div className="flex justify-between">
-          <h1 className="text-4xl font-bold bg-slate-500">Gallerrry</h1>
+          <h1 className="text-4xl font-bold bg-slate-500">Gallery</h1>
           <UploadButton />
         </div>
         <div className="grid grid-cols-4 gap-4">
